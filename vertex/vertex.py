d99b10fa82ea34dd8ae574407dd86193fafa6657
MaxleyCv <maxim.lish@gmail.com>
2020-11-20 01:13:53 +0200

    implemented vertex cell class

--- vertex/vertex.py
+++ vertex/vertex.py
@@ -1,4 +1,10 @@
 class Vertex:
+    """
+    Class for one vertex of the corridor
+    coordinate: tuple of (row, column)
+    tag: symbol of the vertex
+    paths_count: number of possible paths to the vertex, if not initialized it is None
+    """
     def __init__(self, row, column, tag, paths_count):
         self.coordinate = (row, column)
         self.tag = tag
