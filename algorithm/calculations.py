cdfeccf2c959d70e637c583f0eac3965c8017f9e
MaxleyCv <maxim.lish@gmail.com>
2020-11-24 12:04:27 +0200

    optimized algorithm to n**2

--- algorithm/calculations.py
+++ algorithm/calculations.py
@@ -1,40 +1,35 @@
-def get_adjacent_vertexes(current_vertex, tag_coordinates):
-    """
-    Function to find all possible connections to current vertex
-    :param current_vertex: vertex to end at
-    :param tag_coordinates: dictionary of symbol-(list of coordinates) dependency
-    :return: set of coordinates of the vertexes from which it is possible to come to current
-    """
-    result = set()
-    # taking all vertexes with the same tag that are positioned leftside from the current
-    for vertex_coordinate in tag_coordinates[current_vertex.tag]:
-        if vertex_coordinate[1] < current_vertex.coordinate[1]:
-            result.add(vertex_coordinate)
-    # if the column is not at the start we can also get from the left vertex
-    if current_vertex.coordinate[1]:
-        result.add((current_vertex.coordinate[0], current_vertex.coordinate[1] - 1))
-    return result
+from  collections import defaultdict
 
 
-def get_number_of_ways(current, tag_coordinates, map_of_corridor):
+def initialize_paths(map_of_corridor, columns_count, rows_count):
     """
-    Main function that finds number of ways to reach current vertex
-    :param current: Vertex object, current vertex
-    :param tag_coordinates: dictionary: dependency between symbol and a list of coordinates of vertexes with it
-    :param map_of_corridor: list of rows of the vertexes
-    :return: number of pathways to reach the vertex from the leftmost column
+    Function to initialize all the paths to each vertex in map of corridor
+    :param map_of_corridor: matrix with Vertex objects
+    :param columns_count: number of columns
+    :param rows_count: number of rows
+    >>> initialize_paths([], 0, 0)
     """
-    # we should initialize the paths count before changing it
-    if current.paths_count is None:
-        current.paths_count = 0
-    # getting all vertex coordinates from which it is possible to move to current
-    new_vertexes = get_adjacent_vertexes(current, tag_coordinates)
-    # we should add the pathways of all variants, whereas it is not stored we should calculate it
-    for vertex_coordinate in new_vertexes:
-        vertex = map_of_corridor[vertex_coordinate[0]][vertex_coordinate[1]]
-        # if we do not know how many ways there are to reach the vertex we should find it
-        if vertex.paths_count is None:
-            get_number_of_ways(vertex, tag_coordinates, map_of_corridor)
-        # for each already stored value we add it to the current one
-        current.paths_count += vertex.paths_count
-    return current.paths_count
+    # dictionary to store all connections to the left
+    same_tags_paths_leftside = defaultdict(int)
+
+    # literally running through each column
+    for column in range(columns_count):
+        # dictionary to store all values that should be added to next tags
+        additional = defaultdict(int)
+        for row in range(rows_count):
+            # adding of left adjacent element
+            if column > 0:
+                # we should add the adjacent element only whereas it has another tag
+                # otherwise it will be counted in the next section
+                if map_of_corridor[row][column - 1].tag != map_of_corridor[row][column].tag:
+                    map_of_corridor[row][column].paths_count += map_of_corridor[row][column - 1].paths_count
+            # initializing basic value of tag paths (leftmost appearance of the tag)
+            if not map_of_corridor[row][column].tag in same_tags_paths_leftside:
+                same_tags_paths_leftside[map_of_corridor[row][column].tag] = 0
+            # adding all the paths from the tags to the left (we can come from each of them)
+            map_of_corridor[row][column].paths_count += same_tags_paths_leftside.get(map_of_corridor[row][column].tag)
+            # adding to the tags every vertex for next generations
+            additional[map_of_corridor[row][column].tag] += map_of_corridor[row][column].paths_count
+        # updating the tags' paths
+        for key in additional:
+            same_tags_paths_leftside[key] += additional[key]
