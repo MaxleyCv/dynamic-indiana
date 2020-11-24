5ccba1b3cd2cd818d129a25ef3a56f6adb15664e
MaxleyCv <maxim.lish@gmail.com>
2020-11-24 12:07:02 +0200

    implemented unit tests for new algorithm

--- test/calculations_test.py
+++ test/calculations_test.py
@@ -7,37 +7,16 @@ import utils.task_io as io
 
 class TestPathwayAlgorithms(unittest.TestCase):
 
-    def test_adjacent(self):
-        sample_vertex = Vertex(0, 0, 'a', None)
-        sample_tag_coordinates = {'a': {(1, 2), (2, 3)}}
-        self.assertEqual(
-            dynamic.get_adjacent_vertexes(current_vertex=sample_vertex, tag_coordinates=sample_tag_coordinates),
-            set())
-        sample_vertex.coordinate = (1, 3)
-        self.assertEqual(
-            dynamic.get_adjacent_vertexes(current_vertex=sample_vertex, tag_coordinates=sample_tag_coordinates),
-            {(1, 2)})
-        sample_vertex.coordinate = (4, 4)
-        self.assertEqual(
-            dynamic.get_adjacent_vertexes(current_vertex=sample_vertex, tag_coordinates=sample_tag_coordinates),
-            {(1, 2), (2, 3), (4, 3)})
-
-
     def test_findall(self):
         self.samples = ["3 3\naaa\nbac\ndef\n", "7 6\naaaaaaa\naaaaaaa\naaaaaaa\naaaaaaa\naaaaaaa\naaaaaaa",
-                        "10 1\nabcdefahijk\n"]
-        self.results = [5, 201684, 2]
+                        "10 1\nabcdefahij\n"]
+        self.results = [5, 201684, 4]
         for sample_index in range(len(self.samples)):
             corridor = []
-            tag_coordinates = defaultdict(list)
             io.write_result("../io/test1.in", self.samples[sample_index])
-            row_count, column_count = io.get_inputs(tag_coordinates, corridor, "../io/test1.in")
-            dynamic.get_number_of_ways(corridor[0][-1], tag_coordinates, corridor)
-            if row_count != 1:
-                dynamic.get_number_of_ways(corridor[-1][-1], tag_coordinates, corridor)
-                result = corridor[0][-1].paths_count + corridor[-1][-1].paths_count
-            else:
-                result = corridor[0][-1].paths_count
+            row_count, column_count = io.get_inputs("../io/test1.in", corridor)
+            dynamic.initialize_paths(corridor, column_count, row_count)
+            result = corridor[0][-1].paths_count + corridor[-1][-1].paths_count
             self.assertEqual(self.results[sample_index], result)
 
 
--- test/io_test.py
+++ test/io_test.py
@@ -1,26 +1,19 @@
 import unittest
 import utils.task_io as io
 from contextlib import closing
-from collections import defaultdict
 
 
 class IoTest(unittest.TestCase):
     def test_reader(self):
         samples = ["3 3\naaa\nbac\ndef\n", "7 6\naaaaaaa\naaaaaaa\naaaaaaa\naaaaaaa\naaaaaaa\naaaaaaa",
-                        "10 1\nabcdefahijk\n"]
-        results = [[(3, 3), {'a': [(0, 0), (0, 1), (0, 2), (1, 1)], 'b': [(1, 0)], 'c': [(1, 2)], 'd': [(2, 0)], 'e': [(2, 1)], 'f':[(2, 2)]}], [(6, 7)], [(1, 10)]]
+                   "10 1\nabcdefahijk\n"]
+        results = [(3, 3), (6, 7), (1, 10)]
         for sample_index in range(len(samples)):
             corridor = []
-            tag_coordinates = defaultdict(list)
             io.write_result("../io/test1.in", samples[sample_index])
-            sizes = io.get_inputs(tag_coordinates, corridor, "../io/test1.in")
-            self.assertEqual(sizes, results[sample_index][0])
-        corridor = []
-        tag_coordinates = defaultdict(list)
+            sizes = io.get_inputs("../io/test1.in", corridor)
+            self.assertEqual(sizes, results[sample_index])
         io.write_result("../io/test1.in", samples[0])
-        io.get_inputs(tag_coordinates, corridor, "../io/test1.in")
-        self.assertEqual(tag_coordinates, results[0][1])
-
 
     def test_writer(self):
         samples = ["hello", 5, 41, "finally"]
