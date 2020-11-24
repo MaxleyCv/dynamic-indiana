8ad95400c21c52ecec8cffdb392960b8499a5473
MaxleyCv <maxim.lish@gmail.com>
2020-11-24 12:06:24 +0200

    optimized vertex class

--- vertex/vertex.py
+++ vertex/vertex.py
@@ -1,11 +1,9 @@
 class Vertex:
     """
     Class for one vertex of the corridor
-    coordinate: tuple of (row, column)
     tag: symbol of the vertex
     paths_count: number of possible paths to the vertex, if not initialized it is None
     """
-    def __init__(self, row, column, tag, paths_count):
-        self.coordinate = (row, column)
+    def __init__(self, tag, paths_count):
         self.tag = tag
         self.paths_count = paths_count
